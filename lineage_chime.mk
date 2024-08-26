be7f944f9a200748350b0af995ddbb51ef33b32c
Joe7500 <joe75001@gmail.com>
2024-08-26 12:52:06 +0200

    chime: bringup lineage
    Signed-off-by: Joe7500 <joe75001@gmail.com>

--- lineage_chime.mk
+++ lineage_chime.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from chime device
 $(call inherit-product, device/xiaomi/chime/device.mk)
 
-# Inherit some common Bootleggers stuff.
-$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := bootleg_chime
+PRODUCT_NAME := lineage_chime
 PRODUCT_DEVICE := chime
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6115
@@ -23,18 +23,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Flags
-TARGET_BOOTLEG_ARCH := arm64
-WITH_GAPPS := true
-TARGET_USES_BLUR := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_INCLUDE_STOCK_ARCORE := false
-
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Maintainer
-DEVICE_MAINTAINERS := hani~🤍
-BOOTLEGGERS_BUILD_TYPE := Shishufied
+#DEVICE_MAINTAINERS := Joe
--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-# Clone Kernel
-git clone --depth=1 https://github.com/Dominium-Apum/kernel_xiaomi_chime -b android kernel/xiaomi/chime
-
-# Clone Vendor
-git clone https://github.com/Dominium-Apum/vendor_xiaomi_chime vendor/xiaomi/chime
-
-# Clone Hardware
-# rm -rf hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
