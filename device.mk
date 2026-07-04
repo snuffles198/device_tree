56a9b73a20a7716b183862313614a97afc042241
Joe7500 <j7545032@gmail.com>
2026-07-04 19:49:54 +0000

    chime: temp: use old vibrator for now
    Signed-off-by: Joe7500 <j7545032@gmail.com>

--- device.mk
+++ device.mk
@@ -547,8 +547,15 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # Vibrator
+#PRODUCT_PACKAGES += \
+#    vendor.qti.hardware.vibrator.service
+
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.chime \
+    vendor.qti.hardware.vibrator.impl.chime
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vibrator/etc/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_COPY_FILES += \
